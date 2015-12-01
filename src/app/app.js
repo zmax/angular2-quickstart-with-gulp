ce62ee8b986720cbdad5bd0cf78d7790f8225f73
Starck Lin <zenyer@gmail.com>
2015-12-02 06:29:00 +0800

    project init

---
+++ src/app/app.js
@@ -0,0 +1,51 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+// import {bootstrap, Component, FORM_DIRECTIVES, NgClass, NgFor, NgIf} from 'angular2/angular2';
+var angular2_1 = require('angular2/angular2');
+var HEROES = [
+    { "id": 11, "name": "Mr. Nice" },
+    { "id": 12, "name": "Narco" },
+    { "id": 13, "name": "Bombasto" },
+    { "id": 14, "name": "Celeritas" },
+    { "id": 15, "name": "Magneta" },
+    { "id": 16, "name": "RubberMan" },
+    { "id": 17, "name": "Dynama" },
+    { "id": 18, "name": "Dr IQ" },
+    { "id": 19, "name": "Magma" },
+    { "id": 20, "name": "Tornado" }
+];
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'Tour of Heroes';
+        this.heroes = HEROES;
+    }
+    AppComponent.prototype.onSelect = function (hero) { this.selectedHero = hero; };
+    AppComponent.prototype.getSelectedClass = function (hero) {
+        return { 'selected': hero === this.selectedHero };
+    };
+    AppComponent = __decorate([
+        angular2_1.Component({
+            selector: 'my-app',
+            templateUrl: 'templates/app.html',
+            // directives: [FORM_DIRECTIVES, NgClass, NgFor, NgIf],
+            directives: [angular2_1.CORE_DIRECTIVES, angular2_1.FORM_DIRECTIVES],
+            styles: ["\n      .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}\n      .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }\n      .heroes li:hover {color: #369; background-color: #EEE; left: .2em;}\n      .heroes .badge {\n        font-size: small;\n        color: white;\n        padding: 0.1em 0.7em;\n        background-color: #369;\n        line-height: 1em;\n        position: relative;\n        left: -1px;\n        top: -1px;\n      }\n      .selected { background-color: #EEE; color: #369; }\n    "],
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+})();
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+})();
+angular2_1.bootstrap(AppComponent);
+//# sourceMappingURL=app.js.map
---
+++ src/app/app.ts
@@ -0,0 +1,54 @@
+// import {bootstrap, Component, FORM_DIRECTIVES, NgClass, NgFor, NgIf} from 'angular2/angular2';
+import {bootstrap, Component, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/angular2';
+
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'templates/app.html',
+    // directives: [FORM_DIRECTIVES, NgClass, NgFor, NgIf],
+    directives: [CORE_DIRECTIVES, FORM_DIRECTIVES],
+    styles:[`
+      .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
+      .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
+      .heroes li:hover {color: #369; background-color: #EEE; left: .2em;}
+      .heroes .badge {
+        font-size: small;
+        color: white;
+        padding: 0.1em 0.7em;
+        background-color: #369;
+        line-height: 1em;
+        position: relative;
+        left: -1px;
+        top: -1px;
+      }
+      .selected { background-color: #EEE; color: #369; }
+    `],
+})
+class AppComponent {
+    public title = 'Tour of Heroes';
+    public heroes = HEROES;
+    public selectedHero: Hero;
+    onSelect(hero:Hero) { this.selectedHero = hero; }
+    getSelectedClass(hero:Hero) {
+        return { 'selected' : hero === this.selectedHero };
+    }
+}
+
+class Hero {
+    id: number;
+    name: string;
+}
+
+bootstrap(AppComponent);
